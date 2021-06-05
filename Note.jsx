3ba917075579203b106481f26c8e95bc1924059f
Janvi Gupta <67860682+janvigupta38@users.noreply.github.com>
2021-06-05 13:59:39 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This is a Amazing Bootcamp taken up by Shaurya Sinha. The topic covered
+        in the Bootcamp are Javascript,Html & ReactJs(Basic). U will able to
+        make a basic web page from this Bootcamp.I will suggest u to definitly join Bootcamps orginized by ShapeAI.
+      </p>
+    </div>
+  );
+}
+export default Note;
